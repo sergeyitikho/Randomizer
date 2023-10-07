2bb1a18afb58aed8f6af22786af590cd642bd96e
John Doe <sergey@itikho.com>
2023-10-07 15:06:57 +0300

    outlets and action created

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minimumValue: UILabel!
+    @IBOutlet weak var maximumValue: UILabel!
+    @IBOutlet weak var resultValue: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getresultsButton() {
+    }
+    
 }
 
